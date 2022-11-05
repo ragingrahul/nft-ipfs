6a0a4c03d17b2b991a39f08e1ed6e790695cbda0
ragingrahul <raj17sarma@gmail.com>
2022-11-06 00:47:52 +0530

    first commit

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,22 @@
+const {ethers} = require("hardhat");
+require("dotenv").config({path:".env"})
+
+async function main(){
+
+  const metadataURL="ipfs://QmRBDqaMty128ioXnA26m2TfgiKRDcqynJ26mTDnneQnVp/"
+
+  const lw3PunksContract = await ethers.getContractFactory("LW3Punks")
+
+  const deployedLW3PunksContract = await lw3PunksContract.deploy(metadataURL)
+
+  await deployedLW3PunksContract.deployed();
+
+  console.log("LW3Punks Contract Address:",deployedLW3PunksContract.address)
+
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
